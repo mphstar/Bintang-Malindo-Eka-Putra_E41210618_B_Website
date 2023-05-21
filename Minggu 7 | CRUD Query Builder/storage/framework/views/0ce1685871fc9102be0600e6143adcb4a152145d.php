b146112690b450a6ad42bbec6dc9f7334ec940a5
mphstar <bintang.simdig20@gmail.com>
2023-05-21 15:12:23 +0700

    fix minggu 7

--- Minggu 7 | CRUD Query Builder/storage/framework/views/0ce1685871fc9102be0600e6143adcb4a152145d.php
+++ Minggu 7 | CRUD Query Builder/storage/framework/views/0ce1685871fc9102be0600e6143adcb4a152145d.php
@@ -16,7 +16,7 @@
                     <nav aria-label="breadcrumb">
                         <ol class="breadcrumb">
                             <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item"><a href="#">Pengalaman Kerja</a></li>
+                            <li class="breadcrumb-item"><a href="<?php echo e(route('admin.dashboard')); ?>">Pengalaman Kerja</a></li>
                             <li class="breadcrumb-item active" aria-current="page">
                                 <?php echo e(isset($data) ? 'Update' : 'Tambah'); ?>
 
